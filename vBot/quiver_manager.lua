1098c49f5a5777c53feed08baae51e9b0aa89897
Vithrax <adrian.gajdulewicz@outlook.com>
2022-03-17 17:55:35 +0100

    v4.5
    refactored anty-stuck and anty-rs, targetbot RP safe mode, pattern fixes in attackbot and pvp safe when party invite fix, new condition in manager (blacklist player in range), quiver amount label, refactored quiver manager,

--- vBot/quiver_manager.lua
+++ vBot/quiver_manager.lua
@@ -1,111 +1,90 @@
-setDefaultTab("Tools")
-function quiverManager()
-  local panelName = "quiverManager"
-
-  local ui = setupUI([[
-Panel
-  height: 33
-  margin-top: 2
-
-  BotItem
-    id: BoltsID
-    anchors.left: parent.left
-    anchors.top: parent.top
-
-  BotItem
-    id: ArrowsID
-    anchors.left: prev.right
-    anchors.verticalCenter: prev.verticalCenter
-
-  BotSwitch
-    id: BoltsSwitch
-    anchors.top: parent.top
-    anchors.bottom: prev.verticalCenter
-    anchors.right: parent.right
-    text: Sort Bolts
-
-  BotSwitch
-    id: ArrowsSwitch
-    anchors.top: prev.bottom
-    anchors.bottom: ArrowsID.bottom
-    anchors.right: parent.right
-    text: Sort Arrows
-
-  ]]
-  )
-  ui:setId(panelName)
-
-  if not storage[panelName] then
-    storage[panelName] = {
-      arrowsId = 35848,
-      boltsId = 35849,
-      bolts = false,
-      arrows = false
-    }
-  end
-
-  local config = storage[panelName]
-
-  ui.BoltsSwitch:setOn(config.bolts)
-  ui.BoltsSwitch.onClick = function(widget)
-    config.bolts = not config.bolts
-    widget:setOn(config.bolts)
-  end
-  ui.ArrowsSwitch:setOn(config.arrows)
-  ui.ArrowsSwitch.onClick = function(widget)
-    config.arrows = not config.arrows
-    widget:setOn(config.arrows)
-  end
-  ui.BoltsID:setItemId(config.boltsId)
-  ui.BoltsID.onItemChange = function(widget)
-    config.boltsId = widget:getItemId()
-  end
-  ui.ArrowsID:setItemId(config.arrowsId)
-  ui.ArrowsID.onItemChange = function(widget)
-    config.arrowsId = widget:getItemId()
-  end
-
-  local arrows = {16143, 763, 761, 7365, 3448, 762, 21470, 7364, 14251, 3447, 3449, 15793, 25757, 774, 35901}
-  local bolts = {6528, 7363, 3450, 16141, 25758, 14252, 3446, 16142, 35902}
-
-  macro(100, function()
-    local dArrow
-    local dBolt
-    for _, c in pairs(getContainers()) do
-      if not containerIsFull(c) then
-        if c:getContainerItem():getId() == config.arrowsId and config.arrows then
-          dArrow = c
-        elseif c:getContainerItem():getId() == config.boltsId and config.bolts then
-          dBolt = c
-        end
-      end
-    end
-    for _, c in pairs(getContainers()) do
-      if c:getName():lower():find("backpack") or c:getName():lower():find("bag") or c:getName():lower():find("chess") then
-        for _, i in pairs(c:getItems()) do
-          -- arrows
-          if dArrow and config.arrows then
-            if table.find(arrows, i:getId()) and c ~= dArrow then
-              return g_game.move(i, dArrow:getSlotPosition(dArrow:getItemsCount()), i:getCount())
-            end
-          end
-          -- bolts
-          if dBolt and config.bolts then
-            if table.find(bolts, i:getId()) and c ~= dBolt then
-              return g_game.move(i, dBolt:getSlotPosition(dBolt:getItemsCount()), i:getCount())
-            end
-          end
-        end
-      end
-    end
-    delay(900)
-  end)
-end
-
-addSeparator()
 if voc() == 2 or voc() == 12 then
-  UI.Label("[[ Quiver Manager ]]")
-  addSeparator()
-  quiverManager()
-  addSeparator()
+    local bows = { 3350, 31581, 27455, 8027, 20082, 36664, 7438, 28718, 36665, 14246, 19362, 35518, 34150, 29417, 9378, 16164, 22866, 12733, 8029, 20083, 20084, 8026, 8028, 34088}
+    local xbows = { 30393, 3349, 27456, 20085, 16163, 5947, 8021, 14247, 22867, 8023, 22711, 19356, 20086, 20087, 34089}
+    local arrows = { 16143, 763, 761, 7365, 3448, 762, 21470, 7364, 14251, 3447, 3449, 15793, 25757, 774, 35901 }
+    local bolts = { 6528, 7363, 3450, 16141, 25758, 14252, 3446, 16142, 35902 }
+    local hold = false
+
+    onContainerOpen(function(container, previousContainer)
+        hold = false
+    end)
+
+    onContainerClose(function(container)
+        hold = false
+    end)
+    
+    onAddItem(function(container, slot, item, oldItem)
+        hold = false
+    end)
+
+    onRemoveItem(function(container, slot, item)
+        hold = false
+    end)
+
+    onContainerUpdateItem(function(container, slot, item, oldItem)
+        hold = false
+    end)
+
+
+
+    local function manageQuiver(isBowEquipped, quiverContainer)
+        local ammo = isBowEquipped and arrows or bolts
+        local dest = nil
+        local containers = getContainers()
+        for i, container in ipairs(containers) do
+            if container ~= quiverContainer and not containerIsFull(container) then
+                if container:getName():lower():find("backpack") or container:getName():lower():find("bag") or container:getName():lower():find("chess") then
+                    dest = container
+                end
+            end
+        end
+
+        -- clearing
+        if dest then
+            for i, item in ipairs(quiverContainer:getItems()) do
+                if not table.find(ammo, item:getId()) then
+                    local pos = dest:getSlotPosition(dest:getItemsCount())
+                    return g_game.move(item, pos, item:getCount())
+                end
+            end
+        end
+
+        if not containerIsFull(quiverContainer) then
+            for i, container in ipairs(containers) do
+                if container ~= quiverContainer then
+                    for j, item in ipairs(container:getItems()) do
+                        if table.find(ammo, item:getId()) then
+                            local pos = quiverContainer:getSlotPosition(quiverContainer:getItemsCount())
+                            return g_game.move(item, pos, item:getCount())
+                        end
+                    end
+                end
+            end
+        end
+        return true
+    end
+
+    UI.Separator()
+    macro(100, "Quiver Manager", function()
+        if hold then return end -- do nothing if nothing to do
+        local hand = getLeft() and getLeft():getId()
+        local quiverEquipped = getRight() and getRight():isContainer()
+
+        if not hand then return end
+        if not quiverEquipped then return end
+
+        local quiverContainer = getContainerByItem(getRight():getId())
+        if not quiverContainer then return end
+
+        local isBowEquipped = getLeft() and table.find(bows, hand) and true or false
+        if not isBowEquipped then
+            if not table.find(xbows, hand) then
+                return -- neither bow and xbow is equipped
+            end
+        end
+
+        if manageQuiver(isBowEquipped, quiverContainer) then -- if true then it didn't do anything
+            hold = true
+        end
+    end)
 end
