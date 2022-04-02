dd1bfb0ef5f4044e09d8fcf14cdcc87031de485f
tonireinbold <toni.reinbold@gmail.com>
2022-04-02 14:44:56 +0200

    Update extras.lua

--- vBot/extras.lua
+++ vBot/extras.lua
@@ -298,13 +298,15 @@ if true then
     if not settings.oberon then return end
     if mode == 34 then
         if string.find(text, "world will suffer for") then
-            say("Are you ever going to fight or do you prefer talking?")
+            say("Are you ever going to fight or do you prefer talking!")
         elseif string.find(text, "feet when they see me") then
             say("Even before they smell your breath?")
+        elseif string.find(text, "true virtue of") then
+            say("Dare strike up a Minnesang and you will receive your last accolade!")
         elseif string.find(text, "from this plane") then
             say("Too bad you barely exist at all!") 
         elseif string.find(text, "ESDO LO") then
-            say("SEHWO ASIMO, TOLIDO ESD") 
+            say("SEHWO ASIMO, TOLIDO ESD!") 
         elseif string.find(text, "will soon rule this world") then
             say("Excuse me but I still do not get the message!") 
         elseif string.find(text, "honourable and formidable") then
