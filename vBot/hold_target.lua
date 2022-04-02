7c1ea16dd3a9c9e90ee6fbe0fa468a426eff6824
tonireinbold <toni.reinbold@gmail.com>
2022-04-02 15:25:13 +0200

    Update hold_target.lua

--- vBot/hold_target.lua
+++ vBot/hold_target.lua
@@ -1,30 +1,18 @@
 setDefaultTab("Tools")
 
-local targetID = nil
-
--- escape when attacking will reset hold target
-onKeyPress(function(keys)
-    if keys == "Escape" and targetID then
-        targetID = nil
-    end
-end)
-
+-- Hold
 macro(100, "Hold Target", function()
-    -- if attacking then save it as target, but check pos z in case of marking by mistake on other floor
-    if target() and target():getPosition().z == posz() and not target():isNpc() then
-        targetID = target():getId()
-    elseif not target() then
-        -- there is no saved data, do nothing
-        if not targetID then return end
-
-        -- look for target
-        for i, spec in ipairs(getSpectators()) do
-            local sameFloor = spec:getPosition().z == posz()
-            local oldTarget = spec:getId() == targetID
-            
-            if sameFloor and oldTarget then
-                attack(spec)
-            end
-        end
+  if g_game.isAttacking() 
+then
+ oldTarget = g_game.getAttackingCreature()
+  end
+  if (oldTarget and oldTarget:getPosition()) 
+then
+ if (not g_game.isAttacking() and getDistanceBetween(pos(), oldTarget:getPosition()) <= 8) then
+    
+if (oldTarget:getPosition().z == posz()) then
+        g_game.attack(oldTarget)
+      end
     end
-end) 
+  end
+end)
