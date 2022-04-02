21bed4ad730f0f978f0f489acee026f72eba6fde
tonireinbold <toni.reinbold@gmail.com>
2022-04-02 14:57:31 +0200

    Update outmain.lua

--- outmain.lua
+++ outmain.lua
@@ -2,7 +2,7 @@ setDefaultTab("Main")
 
 -- montaria sempre
 
-macro(10000, "Montar Sempre", function()
+macro(10000, "Mount", function()
    local player = g_game.getLocalPlayer()
   if player then 
     player:mount()
