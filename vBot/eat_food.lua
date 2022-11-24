5a977f61048a772dce38c299b2ee35b87aa3ff82
Mathews Tavernari <mathewsjts@gmail.com>
2022-11-24 13:05:03 -0300

    change eat_food macro timer

--- vBot/eat_food.lua
+++ vBot/eat_food.lua
@@ -32,7 +32,7 @@ end, true)
 foodContainer:setHeight(35)
 foodContainer:setItems(storage.foodItems)
 
-macro(500, "Eat Food", function()
+macro(50000, "Eat Food", function()
   if player:getRegenerationTime() > 400 or not storage.foodItems[1] then return end
   -- search for food in containers
   for _, container in pairs(g_game.getContainers()) do
