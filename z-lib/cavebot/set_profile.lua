4b0e908018aac7402dcff2cc76bca99cbb358418
Mathews Tavernari <mathewsjts@gmail.com>
2022-11-24 13:07:25 -0300

    add set profile

---
+++ z-lib/cavebot/set_profile.lua
@@ -0,0 +1,18 @@
+CaveBot.Extensions.SetProfile = {}
+
+CaveBot.Extensions.SetProfile.setup = function()
+	CaveBot.registerAction("SetProfile", "#ffffff", function(value)
+    CaveBot.setCurrentProfile(value)
+    TargetBot.setCurrentProfile(value)
+
+		CaveBot.delay(CaveBot.Config.get("useDelay") + CaveBot.Config.get("ping"))
+		return true
+	end)
+
+	CaveBot.Editor.registerAction("setprofile", "set profile", {
+		value="-Refill",
+		title="Set profile",
+		description="Set CaveBot and TargetBot profile",
+		multiline=false
+	})
+end
