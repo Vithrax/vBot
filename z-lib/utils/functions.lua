18960c938c495f69b9dff7d86be6aa435bf2a183
Mathews Tavernari <mathewsjts@gmail.com>
2022-11-24 13:05:25 -0300

    add utils functions

---
+++ z-lib/utils/functions.lua
@@ -0,0 +1,16 @@
+function toBoolean(str)
+	if str == nil then
+		return false
+	end
+	return string.lower(str) == 'true'
+end
+
+function trimSpaces(str)
+   return str:match("^%s*(.-)%s*$")
+end
+
+function tableLength(T)
+	local count = 0
+	for _ in pairs(T) do count = count + 1 end
+	return count
+end
