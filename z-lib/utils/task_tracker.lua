d79b0a048621d87392ccd0b179e7760c89f7776f
Mathews Tavernari <mathewsjts@gmail.com>
2022-11-29 14:22:16 -0300

    task tracker

--- z-lib/utils/task_tracker.lua
+++ z-lib/utils/task_tracker.lua
@@ -1,3 +1,33 @@
+TaskBot = {}
+TaskBot.loaded = false
+TaskBot.tasksInProgress = {}
+TaskBot.tasksCompleted = {}
+TaskBot.taskProgress = {}
+TaskBot.currentTask = nil
+
+if not storage["playerStatus"] then
+  storage["playerStatus"] = {}
+end
+
+if not storage["playerStatus"][player:getName()] then
+  storage["playerStatus"][player:getName()] = {
+    quests = {},
+    tasks = {}
+  }
+end
+
+if not TaskBot.loaded then
+  local playerTasks = storage["playerStatus"][player:getName()]["tasks"]
+
+  for key, value in pairs(playerTasks) do
+    if value["hunted"] >= value["total"] then
+      table.insert(TaskBot.tasksCompleted, key)
+    end
+  end
+
+  TaskBot.loaded = true
+end
+
 questLogButton = nil
 questLineWindow = nil
 
