ee262545492a327b6c94574140f61cb6539245a3
Mathews Tavernari <mathewsjts@gmail.com>
2022-12-14 10:20:37 -0300

    some fixes

---
+++ z-lib/utils/setup_storage.lua
@@ -0,0 +1,11 @@
+if not storage["playerInfo"] then
+  storage["playerInfo"] = {}
+end
+
+if not storage["playerInfo"][player:getName()] then
+  storage["playerInfo"][player:getName()] = {
+		weaponId = "7434",
+		exerciseWeaponId = "34299",
+		cavebotProfile = "LavaLurkers"
+	}
+end
