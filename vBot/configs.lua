391c4e53ce9ad0fe9a8f21ff1f84ef8984d9e18b
Mathews Tavernari <mathewsjts@gmail.com>
2022-12-14 11:27:53 -0300

    set profiles on each character name folder

--- vBot/configs.lua
+++ vBot/configs.lua
@@ -1,36 +1,26 @@
---[[ 
+--[[
     Configs for modules
-    Based on Kondrah storage method  
+    Based on Kondrah storage method
 --]]
 local configName = modules.game_bot.contentsPanel.config:getCurrentOption().text
 
 -- make vBot config dir
-if not g_resources.directoryExists("/bot/".. configName .."/vBot_configs/") then
-  g_resources.makeDir("/bot/".. configName .."/vBot_configs/")
+if not g_resources.directoryExists("/bot/".. configName .."/vBot_configs/"..player:getName().."/") then
+  g_resources.makeDir("/bot/".. configName .."/vBot_configs/"..player:getName().."/")
 end
-
--- make profile dirs
-for i=1,10 do
-  local path = "/bot/".. configName .."/vBot_configs/profile_"..i
-  if not g_resources.directoryExists(path) then
-    g_resources.makeDir(path)
-  end
-end
-
-local profile = g_settings.getNumber('profile')
 
 HealBotConfig = {}
-local healBotFile = "/bot/" .. configName .. "/vBot_configs/profile_".. profile .. "/HealBot.json"
+local healBotFile = "/bot/" .. configName .. "/vBot_configs/".. player:getName() .."/HealBot.json"
 AttackBotConfig = {}
-local attackBotFile = "/bot/" .. configName .. "/vBot_configs/profile_".. profile .. "/AttackBot.json"
+local attackBotFile = "/bot/" .. configName .. "/vBot_configs/".. player:getName() .."/AttackBot.json"
 SuppliesConfig = {}
-local suppliesFile = "/bot/" .. configName .. "/vBot_configs/profile_".. profile .. "/Supplies.json"
+local suppliesFile = "/bot/" .. configName .. "/vBot_configs/".. player:getName() .."/Supplies.json"
 
 
 --healbot
 if g_resources.fileExists(healBotFile) then
-    local status, result = pcall(function() 
-      return json.decode(g_resources.readFileContents(healBotFile)) 
+    local status, result = pcall(function()
+      return json.decode(g_resources.readFileContents(healBotFile))
     end)
     if not status then
       return onError("Error while reading config file (" .. healBotFile .. "). To fix this problem you can delete HealBot.json. Details: " .. result)
@@ -40,8 +30,8 @@ end
 
 --attackbot
 if g_resources.fileExists(attackBotFile) then
-    local status, result = pcall(function() 
-      return json.decode(g_resources.readFileContents(attackBotFile)) 
+    local status, result = pcall(function()
+      return json.decode(g_resources.readFileContents(attackBotFile))
     end)
     if not status then
       return onError("Error while reading config file (" .. attackBotFile .. "). To fix this problem you can delete HealBot.json. Details: " .. result)
@@ -51,8 +41,8 @@ end
 
 --supplies
 if g_resources.fileExists(suppliesFile) then
-    local status, result = pcall(function() 
-      return json.decode(g_resources.readFileContents(suppliesFile)) 
+    local status, result = pcall(function()
+      return json.decode(g_resources.readFileContents(suppliesFile))
     end)
     if not status then
       return onError("Error while reading config file (" .. suppliesFile .. "). To fix this problem you can delete HealBot.json. Details: " .. result)
@@ -65,7 +55,7 @@ function vBotConfigSave(file)
   --- heal
   --- atk
   --- supply
-  local configFile 
+  local configFile
   local configTable
   if not file then return end
   file = file:lower()
@@ -82,13 +72,13 @@ function vBotConfigSave(file)
     return
   end
 
-  local status, result = pcall(function() 
-    return json.encode(configTable, 2) 
+  local status, result = pcall(function()
+    return json.encode(configTable, 2)
   end)
   if not status then
     return onError("Error while saving config. it won't be saved. Details: " .. result)
   end
-  
+
   if result:len() > 100 * 1024 * 1024 then
     return onError("config file is too big, above 100MB, it won't be saved")
   end
