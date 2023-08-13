4b049115b00803904c6d3e4a5afc7d1c2e04d633
Mathews Tavernari <mathewsjts@gmail.com>
2023-08-13 01:48:06 -0300

    auto change to config script on login

--- z-lib/utils/custom_options.lua
+++ z-lib/utils/custom_options.lua
@@ -4,14 +4,14 @@ macro(500, "pause no bless + aol", function()
 	local aol_id = 3057
 	local hasnt_bless = player:getBlessings() == 0
 	local hasnt_equipped = not getNeck() or getNeck():getId() ~= aol_id
-	local HAS_AOL = itemAmount(aol_id) >= 1
+	local has_aol = itemAmount(aol_id) >= 1
 
-	if hasnt_bless and hasnt_equipped and HAS_AOL then
+	if hasnt_bless and hasnt_equipped and has_aol then
 		g_game.equipItemId(aol_id)
 		return
 	end
 
-	if hasnt_bless and not HAS_AOL then
+	if hasnt_bless and not has_aol then
 		CaveBot.setOff()
 	end
 end)
@@ -20,3 +20,7 @@ UI.Label("CaveBot Profile:")
 UI.TextEdit(storage_custom.cavebot_profile or "", function(widget, text)
 	stg_custom.set_data("cavebot_profile", text)
 end)
+
+TargetBot.setCurrentProfile("__configuration")
+CaveBot.setCurrentProfile("__configuration")
+CaveBot.setOff()
